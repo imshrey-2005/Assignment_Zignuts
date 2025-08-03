6e0da3fec6da4c094c7b556d1ba1a4e651cb08e2
imshrey-2005 <shreysmehta2005@gmail.com>
2025-08-03 12:44:46 +0530

    Js assignment1 added

---
+++ script.js
@@ -0,0 +1,15 @@
+let form=document.getElementById("myform").addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let value=document.getElementById("inputf").value;
+    let reg=/\d/g;
+    let sum=0;
+    let result=value.match(reg);
+    console.log(result)
+    for (x in result){
+        sum += x;
+    }
+    let element = document.createElement('p');
+    element.id='par'
+    element.innerHTML="sum is:" + sum;
+    document.body.appendChild(element)
+})
